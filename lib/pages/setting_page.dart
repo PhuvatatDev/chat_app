8b157545911207ece73daec71098b9fb475d69b2
PhuvatatDev <phuvatat@gmail.com>
2025-04-28 13:01:30 +0700

    feat: add ThemeProvider with dark mode toggle functionality

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,4 +1,7 @@
+import 'package:chat_app/themes/theme_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
@@ -20,6 +23,28 @@ class SettingPage extends StatelessWidget {
             ),
           ),
           centerTitle: true,
+        ),
+        body: Container(
+          decoration: 
+          BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.all(25),
+            padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // dark mode
+              const Text('Dark mode'),
+
+              // switch toggle
+              CupertinoSwitch(
+                value: Provider.of<ThemeProvider>(context, listen: false).isDarkMode, 
+                onChanged: (value) => Provider.of<ThemeProvider>(context, listen: false).toggleTheme(),
+                )
+            ],
+          ),
         ),
     );
   }
---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+    colorScheme: ColorScheme.dark(
+  surface: Colors.blue.shade300,
+  primary: Colors.blue.shade500,
+  secondary: Colors.blueAccent.shade200,
+  tertiary: Colors.white,
+  inversePrimary: Colors.blueGrey.shade900,
+));
